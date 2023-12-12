780304f6bb67f8925d33c0e83e8518ab247f2cbf
victorgalvezsma <victor.galvez@smasoftware.com>
2023-12-12 10:11:32 -0500

    change accept email unique

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\User as AppUser;
+use Exception;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function store(Request $request)
     {
-        $user = new AppUser();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->save();
+
+
+
+        try {
+            $user = new AppUser();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->save();
+        } catch (Exception $e) {
+            return [
+                'success' => false,
+                'message' =>  $e->getMessage()
+            ];
+        }
     }
 }
